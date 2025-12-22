e8487f13ffb9b9052a4cc1a59b65f78c0169d633
jseunghoon9075-svg <jseunghoon9075@gmail.com>
2025-12-22 10:01:25 +0900

    인포카드 데이터불러오기

--- src/index.js
+++ src/index.js
@@ -12,14 +12,12 @@ import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
